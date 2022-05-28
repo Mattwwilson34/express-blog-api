80364e3b46e2ca4dd7fb9a357bf69e04f03d50ff
Matt Wilson <mattwwilson34@gmail.com>
2022-05-28 12:18:43 -0400

    Added create_user route

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,3 +1,10 @@
+// Require models
+const User = require('../models/user');
+
 exports.index = function (req, res) {
   res.send('not implemented yet');
+};
+
+exports.create_user = async function (req, res, next) {
+  res.send('Create User route not implemented');
 };
