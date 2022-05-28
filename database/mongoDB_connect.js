9c2f9bb89e5bc279e809b0eefbb62db541c626cd
Matt Wilson <mattwwilson34@gmail.com>
2022-05-28 16:01:08 -0400

    added find-config lib for recusrive .env finding

--- database/mongoDB_connect.js
+++ database/mongoDB_connect.js
@@ -1,7 +1,7 @@
-require('dotenv').config();
+require('dotenv').config({ path: require('find-config')('.env') });
 const mongoose = require('mongoose');
 
-const connectToDatabase = () => {
+const connectToDatabase = async () => {
   // Set mongoDB connection string using env variables
   const mongoDatabaseUrl = `mongodb+srv://${process.env.USERNAME}:${process.env.PASSWORD}@cluster0.z162m.mongodb.net/?retryWrites=true&w=majority`;
 
@@ -12,20 +12,27 @@ const connectToDatabase = () => {
   };
 
   // Connect to mongoDB
-  mongoose
-    .connect(mongoDatabaseUrl, options)
-    .then(() =>
-      console.log(
-        '\x1b[33m%s\x1b[0m',
-        'Connected to MongoDB express-blog-api cluster0'
-      )
-    );
+  mongoose.connect(mongoDatabaseUrl, options);
 
-  // Bind connection errors so they print to console
+  // Print on success
+  mongoose.connection.on('connected', () =>
+    console.log(
+      '\x1b[33m%s\x1b[0m',
+      'Connected to MongoDB express-blog-api cluster0'
+    )
+  );
+
+  // Handle errors
   mongoose.connection.on(
     'error',
     console.error.bind(console, 'MongoDB connection error:')
   );
+
+  return new Promise((resolve) => {
+    setTimeout(() => {
+      resolve('resolved');
+    }, 1000);
+  });
 };
 
 module.exports = connectToDatabase;
