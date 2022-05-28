e7d33ee9f861b7afa135ff4fa5047f9bba3651ee
Matt Wilson <mattwwilson34@gmail.com>
2022-05-28 13:37:39 -0400

    added formated_date_published vitruals to schema

--- models/blog_post.js
+++ models/blog_post.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose');
+const { DateTime } = require('luxon');
 
 const Schema = mongoose.Schema;
 
@@ -6,9 +7,20 @@ const Blog_PostSchema = new Schema({
   title: { type: String, required: true, maxLength: 100 },
   text: { type: String, required: true, maxLength: 1000 },
   published: { type: Boolean, required: true, default: false },
-  date_published: { type: Date, required: true, default: new Date() },
+  date_published: { type: Date, default: new Date() },
   author: { type: Schema.Types.ObjectId, ref: 'User' },
   comments: [{ type: Schema.Types.ObjectId, ref: 'Comment' }],
+});
+
+//! ===Virtuals===
+
+// Formatted date_published to mm/dd/yyyy
+Blog_PostSchema.virtual('formatted_date_published').get(function () {
+  return DateTime.fromJSDate(this.date_published).toLocaleString({
+    year: 'numeric',
+    month: 'long',
+    day: 'numeric',
+  });
 });
 
 module.exports = mongoose.model('Blog_Post', Blog_PostSchema);
