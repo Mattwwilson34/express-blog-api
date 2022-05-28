7468f78e800ab11c2a2065b3e72e31f56d2e8eff
Matt Wilson <mattwwilson34@gmail.com>
2022-05-28 12:27:07 -0400

    added blog_post and comment routes

--- routes/api.js
+++ routes/api.js
@@ -4,6 +4,56 @@ const router = express.Router();
 // Require controller modules
 const user_controller = require('../controllers/userController');
 
-router.get('/', user_controller.index);
+//! ===USER ROUTES=== //
 
+// Index of all users
+router.get('/user', user_controller.index);
+
+// Get single user
+router.get('/user:id');
+
+// Create user
+router.post('/user', user_controller.create_user);
+
+// Update user
+router.put('/user:id');
+
+// Delete user
+router.delete('user/:id');
+
+//! ===BLOG_POST ROUTES=== //
+
+// Index of all blog_posts
+router.get('/blog_post');
+
+// Get single blog_post
+router.get('/blog_post:id');
+
+// Create blog_post
+router.post('/blog_post');
+
+// Update blog_post
+router.put('/blog_post:id');
+
+// Delete blog_post
+router.delete('blog_post/:id');
+
+//! ===COMMENT ROUTES=== //
+
+// Index of all comments
+router.get('/comment');
+
+// Get single comment
+router.get('/comment:id');
+
+// Create comment
+router.post('/comment');
+
+// Update comment
+router.put('/comment:id');
+
+// Delete comment
+router.delete('comment/:id');
+
+// Export
 module.exports = router;
