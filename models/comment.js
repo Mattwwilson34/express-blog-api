816ddffda5ff4ddea49e3aa21da6bacd42e60115
Matt Wilson <mattwwilson34@gmail.com>
2022-05-28 13:21:56 -0400

    added schema date/time virtuals with luxon

--- models/comment.js
+++ models/comment.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose');
+const { DateTime } = require('luxon');
 
 const Schema = mongoose.Schema;
 
@@ -7,6 +8,21 @@ const CommentSchema = new Schema({
   date_published: { type: Date, required: true, default: new Date() },
   author: { type: Schema.Types.ObjectId, ref: 'User' },
   blog_post: { type: Schema.Types.ObjectId, ref: 'Blog_Post' },
+});
+
+CommentSchema.virtual('date_yyyy_mm_dd').get(function () {
+  return DateTime.fromJSDate(this.date_published).toLocaleString({
+    year: 'numeric',
+    month: 'long',
+    day: 'numeric',
+  });
+});
+
+CommentSchema.virtual('time_hour_min').get(function () {
+  return DateTime.fromJSDate(this.date_published).toLocaleString({
+    hour: 'numeric',
+    minute: '2-digit',
+  });
 });
 
 module.exports = mongoose.model('Comment', CommentSchema);
