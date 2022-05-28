8357653cdd9a166a9dca3939fcd86bc963cb1de4
Matt Wilson <mattwwilson34@gmail.com>
2022-05-28 12:39:27 -0400

    updated blog_post controller post route

---
+++ controllers/blogController.js
@@ -0,0 +1,10 @@
+// Require models
+const Blog_Post = require('../models/blog_post');
+
+exports.index = function (req, res) {
+  res.send('Blog_Post index not implemented yet');
+};
+
+exports.create_blog_post = async function (req, res, next) {
+  res.send('Create User route not implemented');
+};
