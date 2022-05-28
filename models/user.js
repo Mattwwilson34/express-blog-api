c7ab2d39087f929b9fe280356f8216e342023613
Matt Wilson <mattwwilson34@gmail.com>
2022-05-28 13:31:05 -0400

    added formated_date_joined virtual using luxon

--- models/user.js
+++ models/user.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose');
+const { DateTime } = require('luxon');
 
 const Schema = mongoose.Schema;
 
@@ -9,9 +10,20 @@ const UserSchema = new Schema({
   first_name: { type: String, required: true, maxLength: 100 },
   last_name: { type: String, required: true, maxLength: 100 },
   age: { type: Number, required: true },
-  date_joined: { type: Date, required: true },
+  date_joined: { type: Date, default: new Date() },
   blog_posts: [{ type: Schema.Types.ObjectId, ref: 'Blog_Post' }],
   comments: [{ type: Schema.Types.ObjectId, ref: 'Comment' }],
+});
+
+//! ===Virtuals===
+
+// Formatted date_joined to mm/dd/yyyy
+UserSchema.virtual('formatted_date_joined').get(function () {
+  return DateTime.fromJSDate(this.date_joined).toLocaleString({
+    year: 'numeric',
+    month: 'long',
+    day: 'numeric',
+  });
 });
 
 module.exports = mongoose.model('User', UserSchema);
