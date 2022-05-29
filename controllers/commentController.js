42fd986976820f1686b3a7b1a0a02b4bed595bb4
Matt Wilson <mattwwilson34@gmail.com>
2022-05-29 08:22:00 -0400

    Finished all controller except update and create

--- controllers/commentController.js
+++ controllers/commentController.js
@@ -1,11 +1,56 @@
 // Require models
 const Comment = require('../models/comment');
 
-exports.index = function (req, res) {
-  res.send('comment index not implemented yet');
+// Get index of all comments
+exports.index = function (req, res, next) {
+  Comment.find({})
+    .populate('blog_post')
+    .populate('author')
+    .exec(function (err, comments) {
+      if (err) {
+        return next(err);
+      }
+      res.send(comments);
+    });
 };
 
-exports.create_comment = async function (req, res, next) {
-  const comment = new Comment({ text: 'Hello!!' });
-  res.send(comment.time_hour_min);
+// Get comment user via ID
+exports.get_comment = function (req, res, next) {
+  Comment.findById(req.params.id)
+    .populate('blog_post')
+    .populate('author')
+    .exec(function (err, comments) {
+      if (err) {
+        return next(err);
+      }
+      res.send(comments);
+    });
+};
+
+// Create comment
+exports.create_comment = function (req, res, next) {
+  //TODO Once front end is added with update form finish this conroller
+  res.send('Route/Controller to be implemented once front end it ready.');
+};
+
+// Update comment via ID
+exports.update_comment = async function (req, res, next) {
+  const comment = await Comment.findById(req.params.id)
+    .populate('blog_post')
+    .populate('author');
+  //TODO Change what is updated once front end functinality is there
+  comment.text = 'This user has been updated by a /user/:id PUT route';
+  await comment.save();
+  res.send(comment);
+};
+
+// Delete comment via ID
+exports.delete_comment = function (req, res, next) {
+  Comment.findByIdAndDelete(req.params.id, function (err, comment) {
+    if (err) {
+      return next(err);
+    }
+    console.log('Deleted : ', comment);
+    res.send(comment);
+  });
 };
