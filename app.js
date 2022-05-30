27626f92ba870078645a4fc254760fddb0c8c2b7
Matt Wilson <mattwwilson34@gmail.com>
2022-05-30 09:23:38 -0400

    added cors

--- app.js
+++ app.js
@@ -2,6 +2,7 @@
 import express from 'express';
 import {} from 'dotenv/config';
 import morgan from 'morgan';
+import cors from 'cors';
 
 // Start express
 const app = express();
@@ -14,6 +15,7 @@ connectToDatabase();
 import apiRouter from './routes/api.js';
 
 // Application middleware
+app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(morgan('dev'));
