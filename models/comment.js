5cc94aafd273fb9ca36cf26c08d192dacdc9aa2b
Matt Wilson <mattwwilson34@gmail.com>
2022-05-30 22:22:12 -0400

    extended schema to include virtuals on API call

--- models/comment.js
+++ models/comment.js
@@ -3,12 +3,18 @@ import { DateTime } from 'luxon';
 
 const Schema = mongoose.Schema;
 
-const CommentSchema = new Schema({
-  text: { type: String, required: true, maxLength: 500 },
-  date_published: { type: Date, required: true, default: new Date() },
-  author: { type: Schema.Types.ObjectId, ref: 'User' },
-  blog_post: { type: Schema.Types.ObjectId, ref: 'Blog_Post' },
-});
+const CommentSchema = new Schema(
+  {
+    text: { type: String, required: true, maxLength: 500 },
+    date_published: { type: Date, required: true, default: new Date() },
+    author: { type: Schema.Types.ObjectId, ref: 'User' },
+    blog_post: { type: Schema.Types.ObjectId, ref: 'Blog_Post' },
+  },
+  {
+    toObject: { virtuals: true },
+    toJSON: { virtuals: true },
+  }
+);
 
 //! ===Virtuals===
 
