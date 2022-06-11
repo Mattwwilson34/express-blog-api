c61433ad2762d8569fc2791c55778a583cdf81b7
Matt Wilson <mattwwilson34@gmail.com>
2022-06-11 09:20:45 -0400

    Feature: added password hashing to user model
    To improve security, added the bcrypt npm module, a pre-hook to
    user model to hash passwords pre-save, and a user model method to
    decrypt user password hash for validation.

--- models/user.js
+++ models/user.js
@@ -1,4 +1,5 @@
 import mongoose from 'mongoose';
+import bcrypt from 'bcrypt';
 import { DateTime } from 'luxon';
 
 const Schema = mongoose.Schema;
@@ -14,6 +15,29 @@ const UserSchema = new Schema({
   blog_posts: [{ type: Schema.Types.ObjectId, ref: 'Blog_Post' }],
   comments: [{ type: Schema.Types.ObjectId, ref: 'Comment' }],
 });
+
+//! ===Pre-Hooks===
+/**
+ * The code in the UserScheme.pre() function is called a pre-hook.
+ * Before the user information is saved in the database,
+ * `this` function will be called, you will get the plain text password, hash it, and store it.
+ */
+UserSchema.pre('save', async function (next) {
+  const user = this;
+  const hash = await bcrypt.hash(this.password, 10);
+  user.password = hash;
+
+  next();
+});
+
+//! ===Methods===
+// Adds password validation to user model using bcrypt.compare()
+UserSchema.methods.isValidPassword = async function (password) {
+  const user = this;
+  const compare = await bcrypt.compare(password, user.password);
+
+  return compare;
+};
 
 //! ===Virtuals===
 
