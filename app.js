7b813a6e10e3c1f9dd9f17db638e493b4c30e689
Matt Wilson <mattwwilson34@gmail.com>
2022-06-11 09:20:09 -0400

    fix: comment typo

--- app.js
+++ app.js
@@ -20,7 +20,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(morgan('dev'));
 
-// Custome middleware
+// Custom middleware
 
 // Use routes
 app.use('/', apiRouter);
