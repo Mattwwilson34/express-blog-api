68420b5dc18aa49afe8a319efc993ab5cc2e00ee
Matt Wilson <mattwwilson34@gmail.com>
2022-06-19 17:17:30 -0400

    Refactor extracted express server into own file
    Extracted express server into seperate file to reduce
    app.js size and improve readability.

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ const app = express();
 
 // Connect to mongoDB
 import connectToDatabase from './database/mongoDB_connect.js';
-connectToDatabase();
+await connectToDatabase();
 
 //  Require routes
 import apiRouter from './routes/api.js';
@@ -29,9 +29,4 @@ app.use(function (err, req, res) {
   res.json({ error: err });
 });
 
-app.listen(process.env.PORT, () =>
-  console.log(
-    '\x1b[36m%s\x1b[0m',
-    `blog-api listening on port ${process.env.PORT}`
-  )
-);
+export default app;
